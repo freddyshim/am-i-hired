9237322d3035f2b185b09543f3736784fa2e1f00
anookday <anookday@gmail.com>
2021-12-14 20:51:31 +0900

    fixed eslint error

--- components/ContactItem.tsx
+++ components/ContactItem.tsx
@@ -38,7 +38,7 @@ const ContactItem = ({ type }: ContactItemProps) => {
         className={styles.item__link}
         href={link}
         target="_blank"
-        rel="noopener"
+        rel="noopener noreferrer"
       >
         <Image src={src} alt={alt} width="100%" height="100%" />
       </a>
